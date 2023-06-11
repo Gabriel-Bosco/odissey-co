2d13afd354b96b7c4f6eb7280566548073162097
Gabriel Bosco <gabrielboscodev@gmail.com>
2023-06-11 15:08:07 -0300

    Style: Corrigido interação do elemento card de assinatura em telas md.

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 window.addEventListener("scroll", function() {
     var assinatura = document.getElementById("assinatura");
     var surgir = document.querySelector("#element-grower");
+    var surgirmd = document.querySelector("#element-grower-md")
     var assinaturaPosicao = assinatura.getBoundingClientRect().top;
     var windowHeight = window.innerHeight;
   
     if (assinaturaPosicao <= windowHeight) {
       surgir.classList.add("ativo");
+      surgirmd.classList.add("ativo")
     }
   });
